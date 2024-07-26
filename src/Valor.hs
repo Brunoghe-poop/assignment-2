142024d5970d3d77b591298d32402903c5d8933f
Brunoghe-poop <116669600+Brunoghe-poop@users.noreply.github.com>
2024-07-26 15:37:50 -0300

    Update Valor.hs

--- src/Valor.hs
+++ src/Valor.hs
@@ -1,5 +1,8 @@
 module Valor where
 
 valorTotal :: Double -> Double -> Double
-valorTotal morangos maçãs
-  = undefined
+valorTotal morangos macas
+  | morangos <= 5 && macas <= 5 = (morangos * 8.50) + (macas * 5.25)
+  | morangos <= 5 && macas > 5  = (morangos * 8.50) + (macas * 4.75)
+  | morangos > 5  && macas <= 5 = (morangos * 7.20) + (macas * 5.25)
+  | otherwise                   = (morangos * 7.20) + (macas * 4.75)
