41d02fde985d7acaffca28024a4aaa6e7bbf3ab9
Brunoghe-poop <116669600+Brunoghe-poop@users.noreply.github.com>
2024-07-26 16:14:52 -0300

    Update Valor.hs

--- src/Valor.hs
+++ src/Valor.hs
@@ -1,5 +1,9 @@
 module Valor where
 
 valorTotal :: Double -> Double -> Double
-valorTotal morangos maçãs
-  = undefined
+valorTotal morangos macas
+    | morangos < 0 || macas < 0 = error "Quantidade negativa"
+    | otherwise = valorMorangos + valorMacas
+  where
+    valorMorangos = if morangos > 5 then morangos * 7.20 else morangos * 8.50
+    valorMacas    = if macas > 5 then macas * 4.75 else macas * 5.25
