4e2287f4d35db4c1c691f41105050ee6881b0e77
Brunoghe-poop <116669600+Brunoghe-poop@users.noreply.github.com>
2024-07-28 21:22:01 -0300

    Update Valor.hs

--- src/Valor.hs
+++ src/Valor.hs
@@ -1,5 +1,9 @@
 module Valor where
 
 valorTotal :: Double -> Double -> Double
-valorTotal morangos maçãs
-  = undefined
+valorTotal morangos macas =
+  let
+    precoMorangos = if morangos < 5 then 8.5 else 7.2
+    precoMacas = if macas < 5 then 5.25 else 4.75
+  in
+    (precoMorangos * morangos) + (precoMacas * macas)
